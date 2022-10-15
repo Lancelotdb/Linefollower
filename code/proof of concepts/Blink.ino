4f15548a9a4f036284b9e0e99ecc3abffc0b98fe
Lancelotdb <114813824+Lancelotdb@users.noreply.github.com>
2022-10-15 10:57:16 +0200

    Add files via upload

---
+++ code/proof of concepts/Blink.ino
@@ -0,0 +1,19 @@
+/*
+
+Blink proof of concept
+Lancelot De Bruycker
+
+*/
+
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(LED_BUILTIN, 1);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, 0);
+  delay(1000);
+}
